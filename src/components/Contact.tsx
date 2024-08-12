3c6605abfab8e400c1d153dbb288c5ff6161e421
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-08-12 09:45:15 -0300

    alteração funcionalidade contato

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,25 +1,7 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { TextField, Button, Container, Typography, Box } from '@mui/material';
-import emailjs from 'emailjs-com';
 
 export default function Contact() {
-  const form = useRef<HTMLFormElement>(null);
-
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Formulário submetido");
-
-    if (form.current) {
-      emailjs.sendForm('service_t4yqi45', 'template_z967x3u', form.current, '922JgcvAEBKtkOd3Y')
-        .then(() => {
-          alert('Mensagem enviada com sucesso!');
-          form.current!.reset(); 
-        }, (error) => {
-          alert('Erro ao enviar mensagem: ' + JSON.stringify(error));
-        });
-    }
-  };
-
   return (
     <Box sx={{ textAlign: 'center', mb: 8 }}>
       <h3 className="text-4xl font-bold text-white mb-12 text-center text-animation mt-16">
@@ -39,25 +21,16 @@ export default function Contact() {
             Entre em contato comigo enviando uma mensagem usando o formulário abaixo.
         </Typography>
         <form id="contact-form">
-        <TextField
-  label="Nome"
-  fullWidth
-  margin="normal"
-  required
-  InputLabelProps={{ style: { color: 'white' } }}
-  InputProps={{
-    style: { color: 'white' },
-    inputProps: {
-      style: { textTransform: 'uppercase' },
-      onInput: (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.target.value = e.target.value.toUpperCase();
-      }
-    }
-  }}
-  variant="filled"
-  sx={{ bgcolor: 'rgba(255, 255, 255, 0.1)', borderRadius: 1 }}
-/>
-
+          <TextField
+            label="Nome"
+            fullWidth
+            margin="normal"
+            required
+            InputLabelProps={{ style: { color: 'white' } }}
+            InputProps={{ style: { color: 'white' } }}
+            variant="filled"
+            sx={{ bgcolor: 'rgba(255, 255, 255, 0.1)', borderRadius: 1 }}
+          />
           <TextField
             label="Email"
             fullWidth
@@ -91,8 +64,8 @@ export default function Contact() {
                 display: 'flex',
                 justifyContent: 'center',
                 border: '2px solid transparent',
-                borderRadius: '10px',  
-                p: '8px 24px',          
+                borderRadius: '10px',
+                p: '8px 24px',
                 bgcolor: '#4b5563',
                 backdropFilter: 'blur(10px)',
                 color: 'white',
