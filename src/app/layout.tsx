bc60b2fc81e0043da8d4e461fad4887625deecfb
Elias Ribeiro <eliasjunio.riibeiro95@gmail.com>
2024-08-12 10:38:17 -0300

    alteração funcionalidade contato

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,45 +17,25 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* Adiciona o script do Blip Chat aqui */}
-        <script src="https://unpkg.com/blip-chat-widget" async></script>
+        {/* Outros elementos de head, como meta tags, links, etc. */}
       </head>
       <body>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           {children}
         </ThemeProvider>
-        {/* Código do Blip Chatbot */}
+        {/* Adiciona o script do Blip Chat conforme a documentação */}
+        <script src="https://unpkg.com/blip-chat-widget" type="text/javascript"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function() {
-                var minWidthForChatbot = 768;
-                if (window.innerWidth > minWidthForChatbot) {
-                  window.onload = function () {
-                    var blipClient = new BlipChat();
-                    blipClient.withAppKey('cG9ydGlmb2xpb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
-                      .withEventHandler(BlipChat.LOAD_EVENT, function () {
-                        blipClient.sendMessage({
-                          "type": "text/plain",
-                          "content": "Olá"
-                        });
-                      })
-                      .withButton({"color":"#333","icon":"https://blipmediastore.blip.ai/public-medias/Media_a4a37081-2224-47b6-94cf-5aff5fa9cfd2"})
-                      .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
-                      .build();
-
-                    var chatButton = document.getElementById('blip-chat-open-iframe');
-                    chatButton.style.display = 'block';
-
-                    setTimeout(function () {
-                      chatButton.classList.add('blip-chat-visible');
-                    }, 5000);
-
-                    chatButton.addEventListener('click', function () {
-                      chatButton.classList.add('blip-chat-clicked');
-                    });
-                  };
+              (function () {
+                window.onload = function () {
+                    new BlipChat()
+                        .withAppKey('cG9ydGlmb2xpbzg6NjRlYmI3NTYtMTc5Yy00MmIwLWFmZjQtYzM4NGQ3NTFkNzNm')
+                        .withButton({"color":"#333","icon":"https://blipmediastore.blip.ai/public-medias/Media_a4a37081-2224-47b6-94cf-5aff5fa9cfd2"})
+                        .withCustomCommonUrl('https://elias-junio-bqqie.chat.blip.ai/')
+                        .build();
                 }
               })();
             `,
